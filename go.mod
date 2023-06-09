8c0f69249affc00100453ddea18ef1689d0765e3
neaj <neaj@appscode.com>
2023-06-09 15:31:27 +0600

    added-all
    Signed-off-by: neaj <neaj@appscode.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Neaj-Morshad-101/prometheus-apiserver-task
+
+go 1.20
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-   "fmt"
-   "net/http"
+	"fmt"
+	"net/http"
 )
 
-func ping(w http.ResponseWriter, req *http.Request){
-   fmt.Fprintf(w,"pong")
+var pingCounter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "ping_request_count",
+		Help: "No of request handled by Ping handler",
+	},
+)
+
+func ping(w http.ResponseWriter, req *http.Request) {
+	pingCounter.Inc()
+	fmt.Fprintf(w, "pong")
 }
 
 func main() {
-   http.HandleFunc("/ping",ping)
-
-   http.ListenAndServe(":8090", nil)
+	prometheus.MustRegister(pingCounter)
+	http.HandleFunc("/ping", ping)
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":8090", nil)
 }
